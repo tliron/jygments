05d50bf1cdc1d8834aa2a23926fe4c51bc4a5602
Tal Liron <tal.liron@gmail.com>
2013-12-28 16:12:11 +0800

    Update copyright notice for 2014

--- components/jygments/source/com/threecrickets/jygments/grammar/def/StateDef.java
+++ components/jygments/source/com/threecrickets/jygments/grammar/def/StateDef.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright 2010-2014 Three Crickets LLC.
+ * <p>
+ * The contents of this file are subject to the terms of a BSD license. See
+ * attached license.txt.
+ * <p>
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly from Three Crickets
+ * at http://threecrickets.com/
+ */
+
 package com.threecrickets.jygments.grammar.def;
 
 import com.threecrickets.jygments.Def;
